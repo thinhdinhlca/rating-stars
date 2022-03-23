2d0ab42ce239f2d235e69d912e09448052369286
Thịnh Đinh <94897059+thinhdinhlca@users.noreply.github.com>
2022-03-23 08:00:08 +0700

    Update function.js

--- function.js
+++ function.js
@@ -8,47 +8,44 @@ window.function = function (rating) {
   
   let perc = rating/5*100;
   
-  let final = `<div class="ratings">
-   \n \n <div class="empty-stars"></div>
-    \n \n <div class="full-stars" style="width:${perc}%"></div>
-   \n \n </div>
-   \n \n <pre><span><style>
-   \n \n .ratings {
-   \n position: relative;
-   \n vertical-align: middle;
-   \n display: inline-block;
-   \n color: #b1b1b1;
-   \n overflow: hidden;
-\n }
-\n \n.full-stars {
-\n    position: absolute;
-\n    left: 0;
-\n    top: 0;
-\n    white-space: nowrap;
-\n    overflow: hidden;
-\n    color: #fde16d;
-\n }
-\n \n .empty-stars:before, .full-stars:before {
- \n   content:"\2605\2605\2605\2605\2605";
- \n   font-size: 14pt;
-\n }
-\n \n .empty-stars:before {
-\n    -webkit-text-stroke: 1px #848484;
-\n }
-\n \n .full-stars:before {
-\n    -webkit-text-stroke: 1px orange;
-\n }
-
-\n \n @-moz-document url-prefix() {
-\n    .full-stars {
-\n        color: #ECBE24;
-\n    }
-\n }
-
-\n \n .full-stars {
-\n    color: #ECBE24;
-\n }
-`;
+  let final = `<div class="ratings">\n
+<div class="empty-stars"></div>\n
+<div class="full-stars" style="width:${perc}%"></div>\
+</div>\n\n
+<pre><span><style>\n\n
+.ratings {\n
+    position: relative;\n
+    vertical-align: middle;\n
+    display: inline-block;\n
+    color: #b1b1b1;\n
+    overflow: hidden;\n
+}\n \n
+.full-stars { \n
+    position: absolute;\n
+    left: 0;\n
+    top: 0;\n
+    white-space: nowrap;\n
+    overflow: hidden;\n
+    color: #fde16d;\n
+}\n\n
+.empty-stars:before, .full-stars:before {\n
+content:"★★★★★";\n
+font-size: 14pt;\n
+}\n\n
+.empty-stars:before {\n
+    -webkit-text-stroke: 1px #848484;\n
+}\n\n
+.full-stars:before {\n
+    -webkit-text-stroke: 1px orange;\n
+}\n\n
+ @-moz-document url-prefix() {\n
+    .full-stars {\n
+        color: #ECBE24;\n
+    }\n
+}\n\n
+.full-stars {\n
+    color: #ECBE24;\n
+}`;
   
   return final;
 }
